d717a2bcdb537621d4140f721dce4fd6514c169f
Roman <kodochigovra@mail.ru>
2021-05-13 11:52:27 +0300

    Ready tatsks

---
+++ lesson_1/Kodochigov_Roman_dz_3.py
@@ -0,0 +1,8 @@
+# number = int(input("Enter percent: "))
+for number in range(1, 21):
+    if number == 1:
+        print(f'{number} процент')
+    elif number >=2 and number <= 4:
+        print(f'{number} процента')
+    elif number >=5 and number <= 20:
+        print(f'{number} процентов')
